c08e0a7c82067a92bb2e3866ee72920d74bb73d7
MyUsername <romamargine@gmail.com>
2022-03-23 16:25:03 +0100

    add complete excercise

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 //Chiedi all'utente il suo nome
 
+const firstName = prompt('Qual è il tuo nome?');
+console.log(firstName);
 
 //Chiedi all'utente il suo cognome
 
+const lastName = prompt('Qual è il tuo Cognome');
+console.log(lastName);
+
 //Chiedi all'utente il suo colore preferito
 
+const favColor = prompt('Qual è il tuo colore preferito?');
+console.log(FavColro);
 
-//Chiedi all'utente il suo nome
+//Risultato delle info utente 
+//aggiunta di un numero a fianco di info-utente
 
-//Risultato delle info utente
+document.getElementById('info-utente').innerHTML = `${selName+lastName+favColor}22`
+
