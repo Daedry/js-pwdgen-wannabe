a6dcac140d15b088743f6d3e0bb2970f3b7481f0
MyUsername <romamargine@gmail.com>
2022-03-23 14:45:22 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Chiedi all'utente il suo nome
+
+
+//Chiedi all'utente il suo cognome
+
+//Chiedi all'utente il suo colore preferito
+
+
+//Chiedi all'utente il suo nome
+
+//Risultato delle info utente
